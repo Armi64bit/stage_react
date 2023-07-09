0c457345ccbcb7181528ce50b7c1436f93f28868
Armi64bit <bahaeddine.bouzid@esprit.tn>
2023-07-09 16:18:30 +0100

    update
    Signup back and front
    login back  with JWT and refresh token
    using Axios and react-route-dom

---
+++ backend/src/controllers/users.ts
@@ -0,0 +1,95 @@
+import { RequestHandler } from "express";
+import createHttpError from "http-errors";
+import UserModel from "../models/user";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+interface SignUpBody {
+  email?: string;
+  password?: string;
+  username?: string;
+}
+
+interface LoginBody {
+  email?: string;
+  password?: string;
+}
+
+// Generate JWT token
+const generateToken = (payload: any, expiresIn: string): string => {
+  return jwt.sign(payload, 'accessTokenSecret', { expiresIn });
+};
+
+export const signUp: RequestHandler<unknown, unknown, SignUpBody> = async (req, res, next) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const passwordRaw = req.body.password;
+
+  try {
+    if (!username || !email || !passwordRaw) {
+      throw createHttpError(400, "Missing fields");
+    }
+
+    const existingUsername = await UserModel.findOne({ username: username }).exec();
+    if (existingUsername) {
+      throw createHttpError(409, "Username taken");
+    }
+
+    const existingEmail = await UserModel.findOne({ email: email }).exec();
+    if (existingEmail) {
+      throw createHttpError(409, "A user with this Email already exists. Please login instead");
+    }
+
+    const passwordHashed = await bcrypt.hash(passwordRaw, 10);
+
+    const newUser = await UserModel.create({
+      username: username,
+      email: email,
+      password: passwordHashed,
+    });
+
+    const accessToken = generateToken({ userId: newUser._id }, '15m');
+
+    const refreshToken = generateToken({ userId: newUser._id }, '7d');
+
+    res.status(201).json({ user: newUser, tokens: { accessToken, refreshToken } });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login: RequestHandler<unknown, unknown, LoginBody> = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    try {
+      if (!email || !password) {
+        throw createHttpError(400, "Missing email or password");
+      }
+  
+      const user = await UserModel.findOne({ email }).exec();
+      if (!user) {
+        throw createHttpError(401, "Invalid email or password");
+      }
+  
+      console.log('Password:', password);
+      console.log('User Password:', user.password);
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+      if (!isPasswordValid) {
+        throw createHttpError(401, "Invalid email or password");
+      }
+  
+      // Generate access token
+      const accessToken = generateToken({ userId: user._id }, '15m');
+  
+      // Generate refresh token
+      const refreshToken = generateToken({ userId: user._id }, '7d');
+  
+      res.json({ user, tokens: { accessToken, refreshToken } });
+    } catch (error) {
+      console.error(error);
+      next(error);
+    }
+  };
+  
